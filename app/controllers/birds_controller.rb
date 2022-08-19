b11fa41fb8199026c9f2e0bcb7195a879c9a4c3d
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-19 16:55:40 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,27 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # rails magic "under the hood"
+    render json: birds.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: bird
+
+    # option 1: no rails magic
+    # render json: {id: bird.id, name: bird.name, species: bird.species }
+    
+    # option 2: ruby method
+    # render json: bird.slice(:id, :name, :species)
+
+    # option 3: rails magic!
+    if bird
+      render json: bird, only: [:id, :name, :species]
+    else
+      render json: { message: 'Bird not found' }
+    end
+    # OR
+    # render json: bird, except: [:created_at, :updated_at]
   end
 end
